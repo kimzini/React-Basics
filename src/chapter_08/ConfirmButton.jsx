b0ec578cb20cdc2a9686c1c053a950fc2104c973
kimzini <rlawlsdl11111@gmail.com>
2024-08-16 17:57:57 +0900

    chapter08

---
+++ src/chapter_08/ConfirmButton.jsx
@@ -0,0 +1,57 @@
+// 클래스 컴포넌트
+// import React from "react";
+
+// class ConfirmButton extends React.Component {
+//     constructor(props) {
+//         super(props);
+
+//         this.state = {
+//             isConfirmed: false,
+//         };
+
+//         this.handleConfirm = this.handleConfim.bind(this);
+//     }
+
+//     handleConfirm() {
+//         this.setState((prevState) => ({
+//             isConfirmed: !prevState.isConfirmed,
+//         }));
+//     }
+
+//     방법2 : class fields syntax 사용
+//     handleConfirm = () => {
+//         this.setState((prevState) => ({
+//             isConfirmed: !prevState.isConfirmed,
+//         }));
+//     }
+
+//     render() {
+//         return (
+//             <button onClick={this.handleConfirm} disabled={this.state.isConfirmed}>
+//                 {this.state.isConfirmed ? "확인됨" : "확인하기"}
+//             </button>
+//         );
+//     }
+// }
+
+// export default ConfirmButton;
+
+
+//함수 컴포넌트
+import React, { useState } from "react";
+
+function ConfirmButton(props) {
+    const [isConfirmed, setIsConfirmed] = useState(false);
+
+    const handleConfirm = () => {
+        setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
+    };
+
+    return (
+        <button onClick={handleConfirm} disabled={isConfirmed}>
+            {isConfirmed ? "확인됨" : "확인하기"}
+        </button>
+    );
+}
+
+export default ConfirmButton;
