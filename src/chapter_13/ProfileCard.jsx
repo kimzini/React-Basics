1a4a94331ad4c17ab252a4bf660322de5782fb93
kimzini <rlawlsdl11111@gmail.com>
2024-08-23 14:34:53 +0900

    chapter13

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="Jinhee Kim" backgroundColor = "#4ea04e">
+            <p>안녕하세요 김진희입니다.</p>
+            <p>저는 리액트 초보입니다!!!!!!</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
